b86b289a7ffaaea0bafefe6ba37f131096d1a631
Bhavna shriwas <bhavnashriwas851@gmail.com>
2026-04-03 20:05:14 +0530

    prime_number.cpp

--- prime_number.cpp
+++ prime_number.cpp
@@ -2,6 +2,7 @@
 the number that has only two factors is known as prime number
 */
 #include<bits/stdc++.h>
+#include<math.h>
 using namespace std;
 int main(){
     int n;cin>>n;
@@ -17,3 +18,25 @@ int main(){
         cout<<"NO!";
     }return 0;
 }
+/*another approach
+int main()
+{
+    int n ;cin>>n;
+int count=0;
+for(int i=1;i*i<=n;i++)
+{
+if(n%i==0){
+count++;
+  if((n/i)!=i)
+      count++;
+}
+
+}
+if(count==2){
+cout<<"Yes! prime number";
+}
+else{
+cout<<"NO!";
+}
+return 0;
+}*/
